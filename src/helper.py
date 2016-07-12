20e2fa4697534bff9e3db6961fd5a25adbfbea0c
JiYangE <program.ji.yang@gmail.com>
2016-07-12 14:21:14 +0800

    Update read_data_str helper function

--- src/helper.py
+++ src/helper.py
@@ -5,15 +5,14 @@ import urllib.error
 baseURL = 'http://www.neeq.com.cn/'
 
 
-def read_data_str(target):
+def read_data_str(target, values):
     try:
-        response = urllib.request.urlopen(baseURL + target)
-        html = response.read()
-        data_str = html.decode()
-        return data_str
-    except urllib.error.URLError as e:
-        if hasattr(e, "code"):
-            print(e.code)
-        if hasattr(e, "reason"):
-            print(e.reason)
-        return None
+        data = urllib.parse.urlencode(values)
+        data = data.encode('ascii')  # data should be bytes
+        req = urllib.request.Request(baseURL + target, data)
+        with urllib.request.urlopen(req) as response:
+            the_page = response.read().decode()
+    except:
+        print("ERROR")
+    if the_page:
+        return the_page
