66e7ff29ce6887be85d493904ee9cd582b710484
JiYangE <program.ji.yang@gmail.com>
2016-07-13 09:22:37 +0800

    Update db initialization file for creating index of record

--- src/init_db.py
+++ src/init_db.py
@@ -1,21 +1,24 @@
 import mysql.connector
 from mysql.connector import errorcode
+import time
 
 
 TABLES = {}
 
-TABLES["RECORD"] = "\
-    CREATE TABLE RECORD(\
+TABLES["RECORD"] = [
+    "CREATE TABLE RECORD(\
        TYPE_CODE    SMALLINT     NOT NULL,\
        TYPE_NAME    VARCHAR(64)  NOT NULL,\
-       COMP_CODE    VARCHAR(12)  NOT NULL,\
+       COMP_CODE    INTEGER      NOT NULL,\
        COMP_NAME    VARCHAR(32)  NOT NULL,\
        CLASS        VARCHAR(12)  NOT NULL,\
        COMMENT      VARCHAR(128),\
        RECORD_DATE  DATE,\
-       GET_DATE     TIMESTAMP,\
+       GET_DATE     TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,\
        PRIMARY KEY(COMP_CODE)\
-    );"
+    );",
+    "CREATE INDEX record_date_index ON RECORD (RECORD_DATE);"
+]
 
 
 cnx = mysql.connector.connect(user="stock", password="stock123",
@@ -28,8 +31,15 @@ for (table, sql) in TABLES.items():
         print("Dropping table {}... ".format(table), end="")
         cursor.execute("DROP TABLE {};".format(table))
         print("OK")
+    except:
+        print("No table called {}...".format(table))
+    try:
         print("Creating table {}... ".format(table), end="")
-        cursor.execute(sql)
+        cursor.execute(sql[0])
+        print("OK")
+        if len(sql) > 1:
+            cursor.execute(sql[1])
+            print("Index for table {} created...".format(table))
     except mysql.connector.Error as err:
         if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
             print("already exists.")
