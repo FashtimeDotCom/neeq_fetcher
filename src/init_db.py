9d06dc4d63db21687f0d8fb10cc5a328d546ba67
JiYangE <program.ji.yang@gmail.com>
2016-07-13 15:42:46 +0800

    Update db initialization file for creating new table syslog

--- src/init_db.py
+++ src/init_db.py
@@ -15,13 +15,23 @@ TABLES["RECORD"] = [
         COMP_NAME     VARCHAR(16)  NOT NULL,\
         CLASS         SMALLINT     NOT NULL,\
         COMMENT       VARCHAR(128),\
-        POST_DATE     VARCHAR(32),\
+        POST_DATE     DATE,\
         LAST_UPDATED  TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,\
         PRIMARY KEY (ID)\
     );",
     "CREATE INDEX record_date_index ON RECORD (POST_DATE);CREATE INDEX company_class_index ON RECORD (CLASS);"
 ]
 
+TABLES["SYSLOG"] = [
+    "CREATE TABLE SYSLOG(\
+        ID            INTEGER      NOT NULL AUTO_INCREMENT,\
+        MISSION_TYPE  SMALLINT     NOT NULL,\
+        LAST_UPDATED  TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,\
+        STATUS        VARCHAR(16)  NOT NULL,\
+        LOG_DATE      DATE         NOT NULL,\
+        PRIMARY KEY (ID)\
+    );"
+]
 
 cnx = mysql.connector.connect(user="stock", password="stock123",
                               host="192.168.202.161",
@@ -41,7 +51,7 @@ for (table, sql) in TABLES.items():
         print("OK")
         if len(sql) > 1:
             cursor.execute(sql[1])
-            print("Index for table {} created...".format(table))
+            print("Indices for table {} have been created...".format(table))
     except mysql.connector.Error as err:
         if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
             print("already exists.")
