2a36e56d124b533c99ceeaf21d85fc34206d9f67
JiYangE <program.ji.yang@gmail.com>
2016-07-18 12:51:41 +0800

    Add shell scripts for automation fetch

---
+++ fetch.sh
@@ -0,0 +1,5 @@
+task=${1:-}
+start=${2:-}
+end=${3:-}
+
+python3 /Users/Ji/Documents/neeq_fetcher/src/$task.py $start $end
---
+++ init.sh
@@ -0,0 +1,2 @@
+python3 /Users/Ji/Documents/neeq_fetcher/src/init_db.py
+python3 /Users/Ji/Documents/neeq_fetcher/src/init_db_maker.py
